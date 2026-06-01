85d1ce431b02335a4ebb09861f7a92b967cf5bff
aya.akil <72510032@students.liu.edu.lb>
2026-06-01 04:22:45 +0300

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+// pages/Home.js
+import { colors } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="hero-content">
+        <h1>Welcome to <span>Coffee Time</span> Shop</h1>
+        <p>Where you can taste the heaven.</p>
+        <Link to="/menu" className="order-btn">Order Now</Link>
+      </div> 
+    </div>
+  
+  );
+}
+
+export default Home;
---
+++ src/pages/Menu.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
+import Cold from '../menuComponent/Cold.js';
+import Hot from '../menuComponent/Hot.js';
+import Dessert from '../menuComponent/Dessert.js';
+
+function Menu({ addToCart }) {
+  return (
+    <div className="menu-page">
+      <div className="menu-nav">
+        <Link to="/menu">Hot Drinks</Link>
+        <Link to="/menu/cold">Cold Drinks</Link>
+        <Link to="/menu/desserts">Desserts</Link>
+      </div>
+
+      <div className="menu-content">
+        <Routes>
+          {/* الـ path هون بياخد بس التكملة، بدون تكرار كلمة /menu */}
+          <Route path="/" element={<Hot addToCart={addToCart} />} />
+          <Route path="cold" element={<Cold addToCart={addToCart} />} />
+          <Route path="desserts" element={<Dessert addToCart={addToCart} />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
