db61f7cff4fdb6493bf827f0ae7311b6190aa650
aya.akil <72510032@students.liu.edu.lb>
2026-06-04 23:44:56 +0300

    feat: إضافة ميزات وتعديلات جديدة للمشروع

--- src/menuComponent/Hot.js
+++ src/menuComponent/Hot.js
@@ -5,42 +5,42 @@ const hotData = [
         name: 'Flat White',
         price: 3.5,
         desc: 'Smooth espresso blended with a velvety layer of steamed microfoam milk',
-        img: 'images/hot-drinks/flat white.jpg'
+        img: '../public/images/hot-drinks/flat white.jpg'
     },
     {
         id: 2,
         name: 'Frappe',
         price: 4.5,
         desc: 'Rich, frothed instant coffee served warm with a creamy texture',
-        img: 'images/hot-drinks/fappe.jpg'
+        img: '../public/images/hot-drinks/fappe.jpg'
     },
     {
         id: 3,
         name: 'Galão',
         price: 4.5,
         desc: 'Traditional Portuguese hot drink consisting of espresso and three-quarters foamed milk',
-        img: 'images/hot-drinks/Galao.jpg'
+        img: '../public/images/hot-drinks/Galao.jpg'
     },
     {
         id: 4,
         name: 'Irish Coffee',
         price: 4.5,
         desc: 'A warming blend of hot coffee, rich brown sugar, and a thick layer of fresh cream on top',
-        img: 'images/hot-drinks/Irish coffee.jpg'
+        img: '../public/images/hot-drinks/Irish coffee.jpg'
     },
     {
         id: 5,
         name: 'Mocha',
         price: 3.5,
         desc: 'A delicious combination of espresso, steamed milk, and rich chocolate syrup',
-        img: 'images/hot-drinks/mocha.jpg'
+        img: '../public/images/hot-drinks/mocha.jpg'
     },
     {
         id: 6,
         name: 'Vienna Coffee',
         price: 4.5,
         desc: 'Strong espresso topped with a generous dollop of whipped cream instead of milk',
-        img: 'images/hot-drinks/vienna.jpg'
+        img: '../public/images/hot-drinks/vienna.jpg'
     }
 ];
 export default function Hot({ addToCart }) {
